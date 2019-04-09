34a55faff58197e9402698cd9e2797748a87bf4f
Rohan Yadav <drohanyadav@gmail.com>
2019-04-09 17:41:31 +0530

    Add files via upload

--- Hi10Anime.py
+++ Hi10Anime.py
@@ -20,27 +20,38 @@ def Quality(quality):
   try:
     result = chrome.find_element_by_xpath('//*[contains(@id, "{}pane")]'.format(quality))
     chrome.find_element_by_xpath('//*[contains(@id, "{}")]'.format(quality)).click()
-    print('{}p Found'.format(quality))
     return True, result
   except:
     return False, chrome
 
 def search(anime_name):
+  anime_links, anime_names = [], []
   search_url = 'https://hi10anime.com/?s=' + anime_name
   soup = bs(requests.get(search_url).content, 'lxml')
   try:
-    soup =  soup.find('h1', {'class':'entry-title'})
+    soup =  soup.find_all('h1', {'class':'entry-title'})
   except AttributeError:
     print('Anime Not Found')
     return False
-  try:
-    if anime_name.lower() not in soup.find('a').get_text().lower():
-      print('Anime Not Found')
-      return False
-    return soup.find('a')['href']
-  except AttributeError:
-    print('Anime Not Found')
-    return False
+  for anime_page in soup:
+    try:
+      anime_links.append(anime_page.find('a')['href'])
+      anime_names.append(anime_page.get_text())
+    except AttributeError:
+      pass
+  if len(anime_links) > 0:
+    return anime_links, anime_names
+  print('Anime Not Found')
+  return False, False
+
+def close_tabs():
+  while True:
+    try:
+      chrome.switch_to.window(chrome.window_handles[1])
+      chrome.close()
+      chrome.switch_to.window(chrome.window_handles[0])
+    except IndexError:
+      return
 
 def run(anime_link):
   chrome.get(anime_link)
@@ -58,8 +69,7 @@ def run(anime_link):
       except:
         pass
 
-  except Exception as e:
-    print(e)
+  except:
     for quality in ['1080','720','480']:
       result, quality = Quality(quality)
       if result:
@@ -71,14 +81,17 @@ def run(anime_link):
         try:
           a = episodes.find_element_by_xpath('.//a')
           a.click()
+          if len(chrome.window_handles) > 15:
+            close_tabs()
           episode_links.append(format_link(a.get_attribute('data-href')))
         except:
           pass
 
   return episode_links
 
-def idm(episode_links):
+def idm(episode_links, anime_name):
   os.chdir('C:\Program Files (x86)\Internet Download Manager')
+  print('{} : '.format(anime_name))
   for k,link in enumerate(episode_links):
     os.popen('IDMan.exe -a -d {}'.format(link))
     print('Episode {} Added to Queue'.format(k+1))
@@ -90,16 +103,28 @@ def make_file(episode_links, anime_name):
       f.write(link)
       f.write('\n')
 
-scriptname, username, password, *anime_name, result = tuple(sys.argv) 
-	  
+scriptname, username, password, *anime_name, result = tuple(sys.argv)
+
 if scriptname == os.path.basename(__file__):
-  anime_name = ' '.join(anime_name)
-  anime_link = search(anime_name)
-  if anime_link:
+
+  try:
+    anime_name = int(anime_name[0])
+    anime_links = ['https://hi10anime.com/archives/{}'.format(anime_name)]
+    soup = bs(requests.get(anime_links[0]).content, 'lxml')
+    anime_names = [soup.find('h1', {'class':'entry-title'}).get_text()]
+  except ValueError:
+    anime_name = ' '.join(anime_name)
+    anime_links, anime_names = search(anime_name)
+
+  episode_links = []
+  if anime_links:
     with webdriver.Chrome() as chrome:
       login()
-      episode_links = run(anime_link)
-    if result == 'idm':
-      idm(episode_links)
-    elif result == 'txt':
-      make_file(episode_links, sys.argv[3])
+      for anime_link in anime_links:
+        episode_links.append(run(anime_link))
+    print(len(episode_links), len(anime_names))
+    for ep_link, anime_name in zip(episode_links, anime_names):
+      if result == 'idm':
+        idm(ep_link, anime_name)
+      elif result == 'txt':
+        make_file(ep_link, anime_name)
